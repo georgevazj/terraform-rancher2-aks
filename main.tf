c094a50a6812fbca3f27120fedc60724dd1d549f
Jorge <georgevazj@gmail.com>
2021-11-17 12:07:52 +0100

    Removed terraform block

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
 # Configure Rancher provider
-terraform {
-  required_providers {
-    rancher2 = {
-      source = "rancher/rancher2"
-      version = ">=1.21.0"
-    }
-  }
-}
-
 provider "rancher2" {
   alias = "admin"
 
