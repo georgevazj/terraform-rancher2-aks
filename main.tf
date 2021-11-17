462bda837556ff11895cbf5c2c0d88bca1752388
Jorge <georgevazj@gmail.com>
2021-11-17 11:33:53 +0100

    First script revision

--- main.tf
+++ main.tf
@@ -17,17 +17,29 @@ provider "rancher2" {
 }
 
 # Rancher cluster resource
-resource "rancher2_cloud_credential" "cred" {
-  name = "aks_credential"
-  azure_credential_config {
-    client_id       = var.client_id
-    client_secret   = var.client_secret
-    subscription_id = var.subscription_id
-  }
+data "rancher2_cloud_credential" "cred" {
+  name = var.cloud_credential_name
 }
 
 resource "rancher2_cluster" "aks_cluster" {
   name = var.cluster_name
   description = var.cluster_description
+
+  aks_config_v2 {
+    cloud_credential_id = rancher2_cloud_credential.cred.id
+    resource_group = var.resource_group
+    resource_location = var.resource_location
+    dns_prefix = var.cluster_name
+    kubernetes_version = var.kubernetes_version
+    network_plugin = var.network_plugin
+    node_pools {
+      availability_zones = ["1", "2", "3"]
+      name = var.node_pool_name
+      count = var.node_count
+      orchestrator_version = var.kubernetes_version
+      os_disk_size_gb = var.os_disk_size_gb
+      vm_size = var.vm_size
+    }
+  }
 
 }
--- variables.tf
+++ variables.tf
@@ -9,19 +9,10 @@ variable "token_key" {
 }
 
 # Azure credentials
-variable "client_id" {
+variable "cloud_credential_name" {
   type = string
-  description = "(Required) Azure client id"
-}
-
-variable "client_secret" {
-  type = string
-  description = "(Required) Azure client secret"
-}
-
-variable "subscription_id" {
-  type = string
-  description = "(Required) Azure subscription ID"
+  description = "(Required) Rancher cloud credential name"
+  default = "playground-credentials"
 }
 
 # Cluster configuration
@@ -33,4 +24,51 @@ variable "cluster_name" {
 variable "cluster_description" {
   type = string
   description = "(Required) AKS cluster description"
+}
+
+variable "resource_group" {
+  type = string
+  description = "(Required) Azure resource group"
+}
+
+variable "resource_location" {
+  type = string
+  description = "(Required) Azure resource location"
+  default = "westeurope"
+}
+
+variable "kubernetes_version" {
+  type = string
+  description = "(Required) AKS cluster kubernetes version"
+  default = "1.20.9"
+}
+
+variable "network_plugin" {
+  type = string
+  description = "(Required) AKS cluster network plugin"
+  default = "kubenet"
+}
+
+variable "node_pool_name" {
+  type = string
+  description = "(Required) AKS cluster node pool name"
+  default = "agentpool"
+}
+
+variable "node_count" {
+  type = number
+  description = "(Required) AKS cluster node count"
+  default = 1
+}
+
+variable "os_disk_size_gb" {
+  type = number
+  description = "(Required) AKS cluster node OS disk size in GB"
+  default = 128
+}
+
+variable "vm_size" {
+  type = string
+  description = "(Required) AKS cluster node VM size"
+  default = "Standard_DS2_v2"
 }
